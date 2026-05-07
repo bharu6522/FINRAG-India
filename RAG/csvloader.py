5daa824f263339cf2c5395a619ac0edfe1c23231
Bharti Jain <68056115+bharu6522@users.noreply.github.com>
2026-05-07 15:53:00 +0530

    Delete RAG directory

--- RAG/csvloader.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import CSVLoader
-
-loader = CSVLoader(file_path= r"D:\Pinnacle_WorkSpace\CSC_SPV\VenturaSecurities\Data\cob_leads_data_v1\cob_leads_part2_jan2025_sep2025_decr.csv",
-                   encoding= 'utf-8')
-docs = loader.load()
-
-# print(len(docs))
-
-print(docs[0])
