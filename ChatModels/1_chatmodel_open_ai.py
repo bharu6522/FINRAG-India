fbdb4ecb4110fac2e2c1a8c2a0517bf27e39304e
Bharti Jain <68056115+bharu6522@users.noreply.github.com>
2026-05-07 15:50:23 +0530

    Delete ChatModels directory

--- ChatModels/1_chatmodel_open_ai.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import  ChatOpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatOpenAI(model = 'gpt-4.1-nano')
-result = model.invoke("What is my name")
-
-print(result.content)
--- ChatModels/3_chatmodel_google.py
+++
@@ -1,9 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
-result = model.invoke('What is capital of Pakistan')
-print(result.content)
